36def7dc9e52d378709967fdd3083c9c19c7d672
Hasansarwer <hasanmathju@gmail.com>
2024-12-30 15:18:09 +0600

    search function complete

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,6 +1,36 @@
+const searchBtn = document.getElementById('searchBtn');
+const clearBtn = document.getElementById('clearBtn');
+
+
 fetch('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json')
 .then(response => response.json())
 .then(data => {
-  console.log(data);
-  // Do something with your data
+    console.log(Object.keys(data));
 })
+
+function searchDestination(){
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    const searchResults = document.getElementById('searchResults');
+    searchResults.innerHTML ='';
+
+    fetch('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json')
+    .then(response => response.json())
+    .then(data => {
+        const key = Object.keys(data).filter(item => item.includes(input));
+        const destinations = data[key];
+        console.log(destinations);
+
+        destinations.forEach(destination => {
+            console.log(destination);
+            searchResults.innerHTML += `
+            <div class="card">
+                <img src="${destination.image}" alt="Destination Image">
+                <h2>${destination.name}</h2>
+                <p>${destination.description}</p>
+                <button>visit</button>`
+        })
+
+    })
+}
+
+searchBtn.addEventListener('click', searchDestination);
